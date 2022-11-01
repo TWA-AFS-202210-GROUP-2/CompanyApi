43be653e2e498510c3a9630d6b55dc84cb3a40c8
hsun15 <hsun15@slb.com>
2022-11-01 15:34:34 +0800

    finish AC1

---
+++ CompanyApi/Controllers/CompanyContriller.cs
@@ -0,0 +1,22 @@
+﻿using CompanyApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CompanyApi.Controllers
+{
+    [ApiController]
+    [Route("companies")]
+    public class CompanyController
+    {
+        private static List<Company> companies = new List<Company>();
+
+        [HttpPost]
+        public ActionResult<Company> AddNewCompany(Company company)
+        {
+            company.CompanyID = Guid.NewGuid().ToString();
+            companies.Add(company);
+            return new CreatedResult($"/companies/{company.CompanyID}", company);
+        }
+    }
+}
---
+++ CompanyApiTest/Controllers/CompanyControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using CompanyApi.Model;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CompanyApiTest.Controllers
+{
+    public class CompanyControllerTest
+    {
+        [Fact]
+        public async void Should_add_new_company_successfully()
+        {
+            //given
+            var application = new WebApplicationFactory<Program>();
+            var httpClient = application.CreateClient();
+            var company = new Company(name: "SLB");
+            var companyJson = JsonConvert.SerializeObject(company);
+            var postBody = new StringContent(companyJson, Encoding.UTF8, "application/json");
+
+            //when
+            var response = await httpClient.PostAsync("/companies", postBody);
+            //then
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            var createdCompany = JsonConvert.DeserializeObject<Company>(responseBody);
+            Assert.Equal("SLB", createdCompany.Name);
+            Assert.NotEmpty(createdCompany.CompanyID);
+        }
+
+
+    }
+}
